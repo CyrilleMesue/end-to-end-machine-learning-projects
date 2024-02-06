b86f8be0b6383ebe1ab80842a5777640fc4787dd
CyrilleMesue <cyrillemesue@gmail.com>
2024-02-07 02:12:38 +0300

    setup.py and requirements.txt created and package built

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import find_packages, setup
+
+HYPHEN_E_DOT = "-e ."
+def get_requirements(file_path) -> list:
+    """
+    this function will return the list of requirements
+    """
+
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [package.replace("\n", "") for package in requirements]
+
+        if HYPHEN_E_DOT in requirements:
+            requirements.remove(HYPHEN_E_DOT)
+
+    return requirements
+
+
+
+
+setup(
+name = "end-to-end-machine-learning-projects",
+version= "0.0.1",
+author="Cyrille",
+author_email="cyrillemesue@gmail.com",
+packages =  find_packages(),
+install_requires=get_requirements("requirements.txt")
+
+
+)
