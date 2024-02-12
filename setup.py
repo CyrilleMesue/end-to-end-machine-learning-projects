5b7773786b88e5a54a7dc9f40fd3bae5d18b6b4e
CyrilleMesue <cyrillemesue@gmail.com>
2024-02-12 17:34:19 +0300

    corrected error in setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
 from setuptools import find_packages, setup
+from typing import List
 
 HYPHEN_E_DOT = "-e ."
-def get_requirements(file_path) -> list:
+def get_requirements(file_path) -> List[str]:
     """
     this function will return the list of requirements
     """
